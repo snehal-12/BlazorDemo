1590b77a25544f667daded7eda53e54a703a1641
Snehal Gaikwad <sgaikwad@asc.edu>
2025-04-10 11:50:12 -0500

    Web Assembly demo project added

---
+++ BlazorWebAssemblyDemo/BlazorWebAssemblyDemo.Client/Models/Server.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAssemblyDemo.Client.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 2);
+            IsOnline = randomNumber == 0 ? false : true;
+        }
+
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Required]
+        public string? City { get; set; }
+    }
+       
+}
---
+++ BlazorWebAssemblyDemo/BlazorWebAssemblyDemo.Client/Models/ServersRepository.cs
@@ -0,0 +1,83 @@
+﻿namespace BlazorWebAssemblyDemo.Client.Models
+{
+    public class ServersRepository
+    {
+        private static List<Server> servers = new List<Server>()
+        {
+            new Server { ServerId = 1, Name = "Server 1", City = "Madison" },
+            new Server { ServerId = 2, Name = "Server 2", City = "Madison" },
+            new Server { ServerId = 3, Name = "Server 3", City = "Madison" },
+            new Server { ServerId = 4, Name = "Server 4", City = "Madison" },
+            new Server { ServerId = 5, Name = "Server 5", City = "Decatur" },
+            new Server { ServerId = 6, Name = "Server 6", City = "Decatur" },
+            new Server { ServerId = 7, Name = "Server 7", City = "Decatur" },
+            new Server { ServerId = 8, Name = "Server 8", City = "Gadsden" },
+            new Server { ServerId = 9, Name = "Server 9", City = "Gadsden" },
+            new Server { ServerId = 10, Name = "Server 10", City = "Huntsville" },
+            new Server { ServerId = 11, Name = "Server 11", City = "Huntsville" },
+            new Server { ServerId = 12, Name = "Server 12", City = "Selma" },
+            new Server { ServerId = 13, Name = "Server 13", City = "Selma" },
+            new Server { ServerId = 14, Name = "Server 14", City = "Mobile" },
+            new Server { ServerId = 15, Name = "Server 15", City = "Mobile" },
+        };
+
+        public static void AddServer(Server server)
+        {
+            var maxId = servers.Max(s => s.ServerId);
+            server.ServerId = maxId + 1;
+            servers.Add(server);
+        }
+
+        public static List<Server> GetServers() => servers;
+
+        public static List<Server> GetServersByCity(string cityName)
+        {
+            return servers.Where(s => s.City.Equals(cityName, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        public static Server? GetServerById(int serverId)
+        {
+            var server = servers.FirstOrDefault(s => s.ServerId ==serverId);
+            if (server != null)
+            {
+                return new Server
+                {
+                   ServerId = server.ServerId,
+                    Name = server.Name,
+                    City = server.City,
+                    IsOnline = server.IsOnline
+                };
+            }
+
+            return null;
+        }
+
+        public static void UpdateServer(int serverId, Server server)
+        {
+            if (serverId != server.ServerId) return;
+
+            var serverToUpdate = servers.FirstOrDefault(s => s.ServerId == serverId);
+            if (serverToUpdate != null)
+            {
+                serverToUpdate.IsOnline = server.IsOnline;
+                serverToUpdate.Name = server.Name;
+                serverToUpdate.City = server.City;
+            }
+        }
+
+        public static void DeleteServer(int serverId)
+        {
+            var server = servers.FirstOrDefault(s => s.ServerId == serverId);
+            if (server != null)
+            {
+                servers.Remove(server);
+            }
+        }
+
+        public static List<Server> SearchServers(string serverFilter)
+        {
+            return servers.Where(s => s.Name.Contains(serverFilter, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+    }
+}
